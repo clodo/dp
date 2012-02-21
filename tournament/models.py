5a72fbe85c4cc711b6f8d592f778a28053d61222
rustico <nmbases@gmail.com>
2012-02-21 17:55:06 -0300

    Fixture: Se obtiene el actual User: Puede setear el resultado de un partido

--- tournament/models.py
+++ tournament/models.py
@@ -3,14 +3,35 @@ from django.db import models
 class User(models.Model):
     name = models.CharField(max_length = 50)
 
+    def set_match_result(self, match, local_goals, visitor_goals):
+        UserMatchResult.objects.create(user = self,
+                                       match = match, 
+                                       local_goals = local_goals, 
+                                       visitor_goals = visitor_goals)
+
+
 class Team(models.Model):
     name = models.CharField(max_length = 50)
 
 class Fixture(models.Model):
     date = models.DateField()
 
+    class Meta:
+        ordering = ['-date']
+
+    @staticmethod
+    def get_current():
+        return Fixture.objects.all()[0]
+
+
 class Match(models.Model):
     local_team = models.ForeignKey(Team, related_name = "local_team")
     visitor_team = models.ForeignKey(Team, related_name = "visitor_team")
     fixture = models.ForeignKey(Fixture)
+
+class UserMatchResult(models.Model):
+    user = models.ForeignKey(User)
+    match = models.ForeignKey(Match)
+    local_goals = models.PositiveIntegerField()
+    visitor_goals = models.PositiveIntegerField()
 
--- tournament/tests.py
+++ tournament/tests.py
@@ -1,16 +1,64 @@
 from django.test import TestCase
-from tournament.models import Team, Fixture, Match
-from datetime import datetime
+from tournament.models import Team, Fixture, Match, User
+from datetime import datetime, timedelta
 
 class TournamentTest(TestCase):
 
     def test_set_current_fixture(self):
+        # Teams
         team_a = Team.objects.create(name = "Team A")
         team_b = Team.objects.create(name = "Team B")
+
+        # Fixtures
         fixture = Fixture.objects.create(date = datetime.now())
 
+        # Matches
         match = Match.objects.create(local_team = team_a, 
                                      visitor_team = team_b,
                                      fixture = fixture)
 
         self.assertEqual(len(fixture.match_set.all()), 1)
+
+    def test_get_current_fixture(self):
+        # Teams
+        team_a = Team.objects.create(name = "Team A")
+        team_b = Team.objects.create(name = "Team B")
+        team_c = Team.objects.create(name = "Team C")
+
+        # Fixtures
+        current_fixture = Fixture.objects.create(date = datetime.now())
+        past_fixture = Fixture.objects.create(date = (datetime.now() - timedelta(weeks = 1)))
+
+        # Matches
+        match = Match.objects.create(local_team = team_a, 
+                                     visitor_team = team_b,
+                                     fixture = current_fixture)
+
+        match = Match.objects.create(local_team = team_c, 
+                                     visitor_team = team_b,
+                                     fixture = past_fixture)
+
+        match = Match.objects.create(local_team = team_c, 
+                                     visitor_team = team_a,
+                                     fixture = past_fixture)
+
+        self.assertEqual(Fixture.get_current().id, current_fixture.id)
+
+    def test_user_set_fixtures_results(self):
+        # Teams
+        team_a = Team.objects.create(name = "Team A")
+        team_b = Team.objects.create(name = "Team B")
+
+        # Fixtures
+        fixture = Fixture.objects.create(date = datetime.now())
+
+        # Matches
+        match = Match.objects.create(local_team = team_a, 
+                                     visitor_team = team_b,
+                                     fixture = fixture)
+        # User
+        user = User.objects.create(name = "User")
+        user.set_match_result(match, 0, 0)
+
+        self.assertEqual(len(user.usermatchresult_set.all()), 1)
+
